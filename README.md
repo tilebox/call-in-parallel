b84e572c0a4ae1d3a43b351bf0f90152e230f605
Lukas Bindreiter <lukas.bindreiter@tilebox.com>
2024-03-13 10:36:43 +0100

    Prepare for release v0.1.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# call-in-parallel
+
+Run a command multiple times in parallel in multiple processes.
+
+## Usage Example
+
+```bash
+> call-in-parallel -n 3 -- echo Hello World!
+```
+
+## Installation
+
+```
+go install github.com/tilebox/call-in-parallel@latest
+```
--- main.go
+++ main.go
@@ -1,39 +1,76 @@
 package main
 
 import (
+	"context"
 	"github.com/integrii/flaggy"
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
 
+var version = "0.1.0"
+
+// call-in-parallel runs a given command multiple times in parallel.
+// Example Usage:
+// > call-in-parallel -n 3 -- echo hello
 func main() {
-	n := 1
-	flaggy.Int(&n, "n", "", "number of instances to start")
-	flaggy.Parse()
-	if len(flaggy.TrailingArguments) == 0 {
-		log.Fatalln("Supply command after trailing --")
-	}
+	n, delay, command := parseArgs()
 	wg := new(sync.WaitGroup)
 	wg.Add(n)
+
+	// Let's create a context that will be canceled when an interrupt signal is received
+	// That way we can stop starting new commands when the user wants to exit during startup
+	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGTSTP, syscall.SIGQUIT)
+
 	for i := 0; i < n; i++ {
-		time.Sleep(100 * time.Millisecond)
 		go func() {
 			defer wg.Done()
-			err := runCmd(flaggy.TrailingArguments)
+			err := runCmd(command)
 			if err != nil {
 				log.Println("ERROR:", err)
 			}
 		}()
+
+		// avoid time.Sleep() here, since it doesn't interrupt in case of received signals such as SIGINT
+		timer := time.NewTimer(delay)
+		select {
+		case <-ctx.Done():
+			timer.Stop() // interrupt signal received, stop starting commands
+			return
+		case <-timer.C: // the delay time is over, start the next command
+		}
 	}
+	// Wait for all running commands to finish
 	wg.Wait()
 }
 
+// runCmd runs a command and waits for it to finish
+// It returns an error if the command fails to start or run
+// Stdout and Stderr of the command are forwarded to the parent process
 func runCmd(args []string) error {
-	cmd := exec.Command(flaggy.TrailingArguments[0], flaggy.TrailingArguments[1:]...)
+	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
+}
+
+func parseArgs() (int, time.Duration, []string) {
+	flaggy.SetName("call-in-parallel")
+	flaggy.SetDescription("Run a command multiple times in parallel")
+	flaggy.SetVersion(version)
+
+	n := 1
+	delay := 10 * time.Millisecond
+	flaggy.Int(&n, "n", "", "number of instances to start")
+	flaggy.Duration(&delay, "d", "delay", "delay between starting each instance")
+	flaggy.Parse()
+	if len(flaggy.TrailingArguments) == 0 {
+		log.Fatalln("Missing command! Supply command after trailing --. Example: call-in-parallel -n 3 -- echo hello")
+	}
+
+	return n, delay, flaggy.TrailingArguments
 }
